c27a2f2693b0114de3606ca0181e5f06d75e8e81
fize <malzaharguo@gmail.com>
2023-05-10 16:43:28 +0800

    treesitter 配置删除help

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- 不同的语言
-  ensure_installed = { "lua", "vim", "help", "bash", "sql", "toml", "vue",
+  ensure_installed = { "lua", "vim", "bash", "sql", "toml", "vue",
   "json", "yaml", "javascript", "go", "gomod", "gosum", "python", "html", "css"},
 
 
