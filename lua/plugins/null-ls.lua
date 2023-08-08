c63356101a42dd318f0193fe5f9b93c962d47f1b
fize <malzaharguo@gmail.com>
2023-08-08 17:22:02 +0800

    add goimports

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,44 +1,45 @@
 local status, null_ls = pcall(require, "null-ls")
 if not status then
-	vim.notify("null-ls not found")
-	return
+    vim.notify("null-ls not found")
+    return
 end
 
 local formatting = null_ls.builtins.formatting
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		-- Formatting ---------------------
-		--  brew install shfmt
-		formatting.shfmt,
-		formatting.rome,
-		-- StyLua
-		formatting.stylua,
-		formatting.gofmt,
-		-- frontend
-		formatting.prettier.with({ -- 只比默认配置少了 markdown
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"typescript",
-				"typescriptreact",
-				"vue",
-				"css",
-				"scss",
-				"less",
-				"html",
-				"json",
-				"yaml",
-				"graphql",
-			},
-			prefer_local = "node_modules/.bin",
-		}),
-		formatting.fixjson,
-		formatting.black.with({ extra_args = { "--fast" } }),
-	},
-	-- 保存自动格式化
+    debug = false,
+    sources = {
+        -- Formatting ---------------------
+        --  brew install shfmt
+        formatting.shfmt,
+        formatting.rome,
+        -- StyLua
+        formatting.stylua,
+        formatting.gofmt,
+        formatting.goimports,
+        -- frontend
+        formatting.prettier.with({ -- 只比默认配置少了 markdown
+            filetypes = {
+                "javascript",
+                "javascriptreact",
+                "typescript",
+                "typescriptreact",
+                "vue",
+                "css",
+                "scss",
+                "less",
+                "html",
+                "json",
+                "yaml",
+                "graphql",
+            },
+            prefer_local = "node_modules/.bin",
+        }),
+        formatting.fixjson,
+        formatting.black.with({ extra_args = { "--fast" } }),
+    },
+    -- 保存自动格式化
     on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
