22aa7a6af4c3ef6bf376cd394ac4054913b1ee7a
malzahar <malzaharguo@gmail.com>
2025-04-16 17:21:23 +0800

    add fish config

---
+++ config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    set -gx HOMEBREW_NO_AUTO_UPDATE true
+    set -gx DOCKER_BUILDKIT 1
+    set -gx GO111MODULE on
+    set -gx PATH "$HOME/go/bin"
+    set -gx KUBE_EDITOR "lvim"
+    [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
+
+    alias k="kubectl"
+    alias h="hexctl"
+    alias c="ccat"
+
+    alias ghcs="gh copilot suggest"
+    alias ghce="gh copilot explain"
+
+    atuin init fish | source
+end
